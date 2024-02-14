0faf40d60537fe16ffad234fa86ea5a1fb7ad1ab
sophie5612 <49294529+sophie5612@users.noreply.github.com>
2024-02-14 14:37:57 -0500

    REMOVE ALL

--- src/pages/index.js
+++
@@ -1,5 +0,0 @@
-export { default as Err } from './/Err';
-export { default as Home } from './Home/Home';
-export { default as Publications } from './Publications';
-export { default as Research } from './Research';
-export { default as Team } from './Team';
